3c777f4130b79daa7b2488bab7cb5e81a647d96d
steven0129 <h3584935@gmail.com>
2017-08-22 02:21:37 +0800

    add Go to another page Btn

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { Events } from 'ionic-angular'
+import { Events, NavController } from 'ionic-angular'
+import { ContactPage } from '../contact/contact'
 
 @Component({
   selector: 'page-home',
@@ -10,7 +11,7 @@ export class HomePage {
 
   isRound: boolean = true
 
-  constructor(public events: Events) {
+  constructor(public events: Events, private navCtrl: NavController) {
     events.subscribe('log', (text) => {
       console.log(text)
     })
@@ -23,8 +24,12 @@ export class HomePage {
   show(...args) {
     let subTitle = args[0]
     let title = args[1] || 'Warning'
-    let buttons=args[2] || ['OK']
+    let buttons = args[2] || ['OK']
     this.events.publish('presentAlert', title, subTitle, buttons)
+  }
+
+  page() {
+    this.navCtrl.push(ContactPage)
   }
 
   onClick(event) {
